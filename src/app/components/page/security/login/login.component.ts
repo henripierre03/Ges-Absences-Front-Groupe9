aa0b4518af5acc35fef37e5335d18742a2856654
Ousseynou Ndiaye <nousseynou20@gmail.com>
2025-05-31 21:26:18 +0000

    dessign du formulaire

---
+++ src/app/components/page/security/login/login.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule,CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {}
