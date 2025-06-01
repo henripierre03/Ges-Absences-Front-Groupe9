5aee198bc49c68bd31cc7ca463576cdbead3a946
henripierre03 <henripierrebassene@gmail.com>
2025-06-01 10:42:36 +0000

    création du service absence + création admin absence

---
+++ src/app/components/page/absences/admin-absence-list/admin-absence-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-absence-list',
+  imports: [],
+  templateUrl: './admin-absence-list.component.html',
+  styleUrl: './admin-absence-list.component.css'
+})
+export class AdminAbsenceListComponent {
+
+}
