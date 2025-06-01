5ad6046bd60adc5f8a80cf8ba5666332cd038e2f
Ousseynou Ndiaye <nousseynou20@gmail.com>
2025-06-01 12:07:29 +0000

    Recuperation des absence du backend

--- src/app/shared/services/impl/absence.service.ts
+++ src/app/shared/services/impl/absence.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment.development';
+import { AbsenceAndEtudiantResponse } from '../../models/absence.model';
+import { IAbsenceService } from '../IAbsenceService';
+import { GenericService } from './generic.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AbsenceService {
+export class AbsenceService
+  extends GenericService<AbsenceAndEtudiantResponse>
+  implements IAbsenceService
+{
+  constructor(private readonly HttpClient: HttpClient) {
+    super(HttpClient, 'absence');
+  }
 
-  constructor() { }
+  getAllAbsences(): Observable<AbsenceAndEtudiantResponse[]> {
+    return this.httpClient.get<AbsenceAndEtudiantResponse[]>(
+      `${environment.apiUrl}/absence`
+    );
+  }
 }
