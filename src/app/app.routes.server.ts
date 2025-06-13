719f142a94fa031dfa6f490a6b328342e9952519
Ousseynou Ndiaye <nousseynou20@gmail.com>
2025-06-13 18:50:44 +0000

    modification

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,19 +1,19 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: 'absence/justification/:absenceId',
-    renderMode: RenderMode.Prerender,
-    // Ajoutez ici la fonction getPrerenderParams
-    getPrerenderParams: async () => {
-      // TODO: Retournez un tableau de paramètres à pré-rendre
-      // Exemple statique :
-      return [{ absenceId: '6840735758950d04348206f0' }, { absenceId: '6840735558950d04348206dc' }];
-      // En production, vous pouvez aller chercher la liste des IDs depuis une API ou une base de données
-    },
-  },
+  // {
+  //   path: 'absence/justification/:absenceId',
+  //   renderMode: RenderMode.Prerender,
+  //   // Ajoutez ici la fonction getPrerenderParams
+  //   getPrerenderParams: async () => {
+  //     // TODO: Retournez un tableau de paramètres à pré-rendre
+  //     // Exemple statique :
+  //     return [{ absenceId: '6840735758950d04348206f0' }, { absenceId: '6840735558950d04348206dc' }];
+  //     // En production, vous pouvez aller chercher la liste des IDs depuis une API ou une base de données
+  //   },
+  // },
   {
     path: '**',
-    renderMode: RenderMode.Prerender,
+    renderMode: RenderMode.Client,
   },
 ];
