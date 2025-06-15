204b0e2eaa97135712d7c14a8c85b1c3587bb9da
Ousseynou Ndiaye <nousseynou20@gmail.com>
2025-06-15 22:41:52 +0000

    modification de route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,52 +1,54 @@
 import { Routes } from '@angular/router';
+import { AbsenceComponent } from './components/page/absences/absence.component';
+import { DashboardComponent } from './components/page/absences/dashboard/dashboard.component';
 import { PageAbscenceComponent } from './components/page/absences/page-abscence/page-abscence.component';
 import { PageJustificationComponent } from './components/page/absences/page-justification/page-justification.component';
-import { AbsenceComponent } from './components/page/absences/absence.component';
-import { SecurityComponent } from './components/page/security/security.component';
-import { LoginComponent } from './components/page/security/login/login.component';
 import { AuthGuard } from './components/page/security/auth.guard';
-import { DashboardComponent } from './components/page/absences/dashboard/dashboard.component';
+import { LoginComponent } from './components/page/security/login/login.component';
+import { SecurityComponent } from './components/page/security/security.component';
 
 export const routes: Routes = [
-    {
-        path: 'absence',
-        component: AbsenceComponent ,
-        canActivate:[AuthGuard],
-
-        children: [
-            {
-                path: '',
-                component: PageAbscenceComponent,
-            },
-            {
-                path: 'accueil',
-                component: DashboardComponent
-            },
-            {
-                path: "justification/:absenceId",
-                component: PageJustificationComponent,
-                data: { renderMode: 'server' }
-            }
-        ]
-    },
-    {
-        path: 'security',
-        component: SecurityComponent,
-        children:[
-            {
-                path:'',
-                component: LoginComponent
-            }
-        ]
-    },
-    {
+  {
+    path: 'security',
+    component: SecurityComponent,
+    children: [
+      {
         path: '',
-        redirectTo: '/security',
-        pathMatch: 'full' 
-    },
+        component: LoginComponent,
+      },
+    ],
+  },
 
-    {
-        path: '**',
-        redirectTo: '/absence',
-    }
+  {
+    path: 'absence',
+    component: AbsenceComponent,
+    canActivate: [AuthGuard],
+
+    children: [
+      {
+        path: '',
+        component: PageAbscenceComponent,
+      },
+      {
+        path: 'accueil',
+        component: DashboardComponent,
+      },
+      {
+        path: 'justification/:absenceId',
+        component: PageJustificationComponent,
+        data: { renderMode: 'server' },
+      },
+    ],
+  },
+
+  {
+    path: '',
+    redirectTo: '/security',
+    pathMatch: 'full',
+  },
+
+  {
+    path: '**',
+    redirectTo: '/absence',
+  },
 ];
